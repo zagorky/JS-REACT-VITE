ef264ebefd3d33bad7c32e129b2118ad290d4a79
zagorky <zagorkiynahyi@gmail.com>
2024-06-15 15:40:32 +0300

    axios

--- src/components/ProductCatalog/ProductCatalog.tsx
+++ src/components/ProductCatalog/ProductCatalog.tsx
@@ -2,24 +2,40 @@ import { ProductItem } from "@/types";
 import classes from "./ProductCatalog.module.scss";
 import { Product } from "@/components";
 import { useEffect, useState } from "react";
+import { api } from "@/app/api";
 
 const ProductCatalog = () => {
   const [products, setProducts] = useState<ProductItem[]>([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [isError, setIsError] = useState(false);
+
+  const [error, setError] = useState("");
+
   const getProduct = async () => {
+    setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:3000/products");
-      const data = await response.json();
+      const { data } = await api.get("/products");
       setProducts(data);
     } catch (e) {
       console.error(e);
+      setError(e as string);
+      setIsError(true);
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
     getProduct();
   }, []);
 
+  if (isLoading) {
+    return <h1>Загрузка</h1>;
+  }
+  if (isError) {
+    return <h1>Ошибка {error}</h1>;
+  }
   return (
     <>
       <div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CartProvider, CounterProvider, ThemesProvider } from "./context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CartProvider>
-      <CounterProvider>
-        <ThemesProvider>
-          <App />
-        </ThemesProvider>
-      </CounterProvider>
-    </CartProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <CounterProvider>
+          <ThemesProvider>
+            <App />
+          </ThemesProvider>
+        </CounterProvider>
+      </CartProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
